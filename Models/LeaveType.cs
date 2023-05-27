1616f22a29dc186dff46c20b81e478d1b83e2ce0
Mamady Konate <L00177597@atu.ie>
2023-05-27 17:03:16 +0100

    Adding classes from older project

---
+++ Models/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace emsfinal3.Models
+{
+    public class LeaveType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = default;
+    }
+}
---
+++ Models/UserLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace emsfinal3.Models
+{
+    public class UserLogin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
